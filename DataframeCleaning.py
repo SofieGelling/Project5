3f68b95ca0b2d8b452ac331aa9c9bab0be56f680
SofieGellings <Sofie@Gellings.com>
2024-10-31 14:04:36 +0100

    vertalen

--- DataframeCleaning.py
+++ DataframeCleaning.py
@@ -7,7 +7,12 @@ dienstregeling = pd.read_excel("Connexxion data - 2024-2025.xlsx", sheet_name="D
 def omloopplanningEngels(bus_schedule):
     result = bus_schedule.copy()
     result = result.drop('Unnamed: 0', axis=1) #zodat de index niet dubbel wordt laten zien in de tool
-    result = result.rename(columns={'startlocatie': 'Start Location', 'eindlocatie': 'End Location', 'starttijd': 'Start Time', 'eindtijd': 'End Time', 'activiteit': 'Activity', 'buslijn': 'Bus Line', 'energieverbruik': 'Energy consumption', 'starttijd datum': 'Start Time Date', 'eindtijd datum': 'End Time Date', 'omloop nummer': 'Bus Number'})
+    result = result.rename(columns={'startlocatie': 'Start Location', 'eindlocatie': 'End Location', 'starttijd': 'Start Time', 'eindtijd': 'End Time', 'activiteit': 'Activity', 'buslijn': 'Bus Line', 'energieverbruik': 'Energy consumption', 'starttijd datum': 'Start Time Date', 'eindtijd datum': 'End Time Date', 'omloop nummer': 'Bus Number', 'afstand in meters': 'disctance in meters', 'Huidige energie': 'current energy'})
+    return result
+
+def omloopplanning_vertalen(bus_schedule):
+    result = bus_schedule.copy()
+    result = result.rename(columns={'startlocatie': 'Start Location', 'eindlocatie': 'End Location', 'starttijd': 'Start Time', 'eindtijd': 'End Time', 'activiteit': 'Activity', 'buslijn': 'Bus Line', 'energieverbruik': 'Energy consumption', 'starttijd datum': 'Start Time Date', 'eindtijd datum': 'End Time Date', 'omloop nummer': 'Bus Number', 'afstand in meters': 'Disctance in meters', 'Huidige energie': 'Current energy', 'energieverbruik nieuw': 'Energy consumption'})
     return result
 
 def afstandsmatrixEngels(distance_matrix):
